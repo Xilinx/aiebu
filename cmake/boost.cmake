635a9ac9e1a4da0df8ba525597dc90f94ddc253e
Sonal Santan <sonal.santan@amd.com>
2025-11-21 15:48:55 -0800

    Resolve build failure with CMake requesting regex component on Ubuntu 24.04
    Signed-off-by: Sonal Santan <sonal.santan@amd.com>

--- cmake/boost.cmake
+++ cmake/boost.cmake
@@ -8,15 +8,13 @@ endif()
 # Boost.Regex is header-only from 1.76.0+ (when using C++11 or later)
 # Require minimum Boost 1.76.0 for header-only Boost.Regex support
 if(${CMAKE_VERSION} VERSION_LESS "3.30")
-  find_package(Boost 1.76.0 REQUIRED COMPONENTS regex)
+  find_package(Boost 1.76.0 REQUIRED)
 else()
-  find_package(Boost 1.76.0 CONFIG REQUIRED COMPONENTS regex)
+  find_package(Boost 1.76.0 CONFIG REQUIRED)
 endif()
 
-message(STATUS "Boost version: ${Boost_VERSION}")
-message(STATUS "Boost include dir: ${Boost_INCLUDE_DIRS}")
-message(STATUS "Boost libraries: ${Boost_LIBRARIES}")
-message(STATUS "Boost.Regex: using header-only mode (Boost >= 1.76.0)")
+message("-- Boost version: ${Boost_VERSION}")
+message("-- Boost include dir: ${Boost_INCLUDE_DIRS}")
 
 # Some later versions of boost spews warnings form property_tree
 # but can be disabled with this setting
