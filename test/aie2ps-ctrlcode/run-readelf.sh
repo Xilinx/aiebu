38853ff70e00336cfaa2a0917cfc388b779bcc92
Sonal Santan <sonal.santan@amd.com>
2026-04-13 16:43:48 -0700

    Cursor helped readelf post processing for ancient OS like RHEL 8.x
    Signed-off-by: Sonal Santan <sonal.santan@amd.com>

--- test/aie2ps-ctrlcode/run-readelf.sh
+++ test/aie2ps-ctrlcode/run-readelf.sh
@@ -9,6 +9,8 @@
 #   W (write), A (alloc), X (execute), M (merge), S (strings), I (info),
 #   L (link order), N (extra OS processing required), G (group), T (TLS),
 #   C (compressed), O (ordered), R (GNU retain), E (exclude)
+#
+# Normalize ELF header Machine: to match gold files (col_0.gold et al.),
+# which expect WE32100 instead of the eu-readelf name "m32".
 
-
-eu-readelf -a $1 | sed "/Key to Flags/,+3d" > $2
+eu-readelf -a $1 | sed "/Key to Flags/,+3d" | sed '/^[[:space:]]*Machine:/s/m32/WE32100/' > "$2"
