c108cc4bc7d68021b53bac08235258512d24188c
AdvaitNaik <65186453+AdvaitNaik@users.noreply.github.com>
2026-06-26 15:11:41 -0700

    dynamic tracing compiler result buffer API refactor (#310)
    * refactor dtrace result buffer API and remove nlohmann dependency
    
    Signed-off-by: advanaik <AdvaitHemant.Naik@amd.com>
    
    * address comments
    
    Signed-off-by: advanaik <AdvaitHemant.Naik@amd.com>
    
    * address more comments
    
    Signed-off-by: advanaik <AdvaitHemant.Naik@amd.com>
    
    ---------
    
    Signed-off-by: advanaik <AdvaitHemant.Naik@amd.com>

--- src/cpp/dtrace/dtrace.cpp
+++ src/cpp/dtrace/dtrace.cpp
@@ -1,16 +1,19 @@
 // SPDX-License-Identifier: MIT
-// Copyright (C) 2024-2025 Advanced Micro Devices, Inc. All rights reserved.
+// Copyright (C) 2024-2026 Advanced Micro Devices, Inc. All rights reserved.
 
-// This file implements the dtrace public APIs for creating dtrace control buffer, 
+// This file implements the dtrace public APIs for creating dtrace control buffer,
 // dtrace memory buffer and dtrace result file.
 #include "dtrace.h"
+#include "utils.h"
+
 #include "control/control.h"
 
 #include <cstring>
 #include <exception>
-#include <filesystem>
+#include <iostream>
 #include <memory>
-#include <stdexcept>
+#include <unordered_map>
+#include <vector>
 
 /**
  * @struct dtrace_buffer_info
@@ -286,10 +289,8 @@ get_dtrace_result_file(dtrace_handle_t dtrace_handle, const std::string& result_
     }
 }
 
-/*
-void
-update_dtrace_result_buffer(dtrace_handle_t dtrace_handle, const std::string& result_key,
-    nlohmann::ordered_json& result_buffer)
+std::string
+get_dtrace_result_buffer(dtrace_handle_t dtrace_handle)
 {
     try
     {
@@ -328,10 +329,9 @@ update_dtrace_result_buffer(dtrace_handle_t dtrace_handle, const std::string& re
             mem_buffers[uC_index] = std::move(mem_buffer);
         }
 
-        // Update the result buffer with the given result key and result buffer data
+        // Create the result buffer as JSON object
         nlohmann::ordered_json result_json = nlohmann::ordered_json::object();
         handle->g_control->create_result_buffer(result_buffers, mem_buffers, result_json);
-        result_buffer[result_key] = std::move(result_json);
 
         // Iterate over and copy back modified buffers for each uC in global map
         for (const auto& [uC_index, l_dtrace_buffer_info] : handle->g_dtrace_buffer_info_map)
@@ -351,13 +351,16 @@ update_dtrace_result_buffer(dtrace_handle_t dtrace_handle, const std::string& re
                 buffer.size() * sizeof(uint32_t)
             );
         }
+
+        // Return the result buffer as JSON string
+        return result_json.dump();
     }
     catch (const std::exception& e)
     {
         std::cerr << e.what();
+        return "null";
     }
 }
-*/
 
 void
 destroy_dtrace_handle(dtrace_handle_t dtrace_handle)
